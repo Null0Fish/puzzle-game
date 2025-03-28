5a161c0a5bc00a1e742e5ff2cc4f2b99448cef9e
Null0Fish <jackson_connors@icloud.com>
2025-03-28 19:57:20 -0400

    deleted old home gui screnes and scripts now working on level gui :(

--- content/gui/level_gui.gd
+++ content/gui/level_gui.gd
@@ -4,7 +4,7 @@ func get_guis():
 	return [$AdjacentGUI, $DiagonalGUI, $FullGUI]
 
 func _on_home_button_pressed():
-	get_tree().change_scene_to_file("res://content/gui/home_screen.tscn")
+	get_tree().change_scene_to_file("res://content/gui/start_screen.tscn")
 
 # currently removed as level screen has been joined with start one
 func _on_level_button_pressed():
