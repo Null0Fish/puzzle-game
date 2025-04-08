b907b72d5b76d04ea0c0caa8ee8bc0f08a0727ba
Null0Fish <jackson_connors@icloud.com>
2025-04-08 18:12:14 -0400

    worked on question mark gui

--- content/gui/question_gui.gd
+++ content/gui/question_gui.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var panel: Panel = $Panel
+@onready var panel: Panel = $VBoxContainer/CenterContainer/Panel
+@onready var label: Label = $VBoxContainer/Label
 
 var style_box_flat: StyleBoxFlat = StyleBoxFlat.new()
 var is_active_panel: bool = false
@@ -10,8 +11,10 @@ func _process(_delta: float) -> void:
 
 func _update_panel_style():
 	if is_active_panel:
+		label.show()
 		style_box_flat.bg_color = Color(0.130728, 0.159803, 0.357762)
 	else:
+		label.hide()
 		style_box_flat.bg_color = Color(0.133, 0.125, 0.204)
 	panel.add_theme_stylebox_override("panel", style_box_flat)
 
@@ -19,4 +22,3 @@ func _update_panel_style():
 func _on_question_button_pressed() -> void:
 	is_active_panel = not is_active_panel
 	_update_panel_style()
-	print("you pressed a 	uestion makr lol")
